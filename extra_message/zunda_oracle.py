d922dc5bc6eb011509c6067e258dd8992aaf4316
Riku Ishikawa <strv@strv.dev>
2023-04-13 02:15:33 +0900

    feat: 知識をギルドごとに分離

--- extra_message/zunda_oracle.py
+++ extra_message/zunda_oracle.py
@@ -6,7 +6,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 # db = FAISS.from_texts(texts=[], embedding=OpenAIEmbeddings(openai_api_key=env_token.OPENAI_TOKEN))
 
@@ -15,18 +15,14 @@ class ZundaOracleExtraMessage(ExtraMessage):
     embedding: OpenAIEmbeddings
     chain: LLMChain
     wake_word: str
-    faiss_indexes: FAISS
+    faiss_indexes: Dict[int, FAISS]
 
     def __init__(self, wake_word: str, api_key: str) -> None:
         super().__init__()
         self.wake_word = wake_word
         self.llm = ChatOpenAI(temperature=0.7, openai_api_key=api_key)
         self.embedding = OpenAIEmbeddings(openai_api_key=api_key)
-
-        try:
-            self.faiss_indexes = FAISS.load_local('zunda_oracle.faiss', self.embedding)
-        except:
-            self.faiss_indexes = None
+        self.faiss_indexes = {}
 
         question_prompt = ChatPromptTemplate.from_messages([
                 HumanMessagePromptTemplate(prompt = PromptTemplate(template=''
@@ -42,6 +38,7 @@ class ZundaOracleExtraMessage(ExtraMessage):
                     'else if 「ずんだもんの知識:」が含まれていない:'
                     '    # この場合、ずんだもんの知識について言及してはなりません'
                     '    print("[「質問:」に対してのみ返答]")'
+                    '画像を貼ってと言われたときには、資料に含まれるURLが画像として振る舞うため、それを出力するべきです'
                     '{input}。では、ずんだもんとしての口調を意識し、必ず回答の本文だけを出力してください。絶対にあなたの出力は回答の本文のみです。', input_variables=['input']))
                 ])
         self.chain = LLMChain(llm=self.llm, prompt=question_prompt)
@@ -60,22 +57,34 @@ class ZundaOracleExtraMessage(ExtraMessage):
 
     def get_extra_message(self, message: discord.Message) -> str:
         query = message.content[len(self.wake_word):]
-        documents_and_scores = self.faiss_indexes.similarity_search_with_score(query, k=10)
 
-        # 合計1000文字まで資料を結合
-        con_doc = ''
-        for doc, score in documents_and_scores:
-            if score > 0.35:
-                break
-            if len(con_doc) < 1000:
-                con_doc += doc.page_content
+        guild = message.guild.id
+        faiss_path = f"zunda_oracle.faiss/{guild}"
+        if self.faiss_indexes.get(guild) is None:
+            try:
+                self.faiss_indexes[guild] = FAISS.load_local(faiss_path, self.embedding)
+            except:
+                self.faiss_indexes[guild] = None
 
-        if con_doc != '':
-            con_doc = f'ずんだもんの知識:{con_doc}\n'
+        if self.faiss_indexes[guild] is not None:
+            documents_and_scores = self.faiss_indexes[guild].similarity_search_with_score(query, k=10)
 
-        final_query = f"{con_doc}質問: {query}"
+            # 合計1000文字まで資料を結合
+            con_doc = ''
+            for doc, score in documents_and_scores:
+                if score > 0.35:
+                    break
+                if len(con_doc) < 1000:
+                    con_doc += doc.page_content
 
-        print(con_doc)
+            if con_doc != '':
+                con_doc = f'ずんだもんの知識:{con_doc}\n'
+
+            final_query = f"{con_doc}質問: {query}"
+
+            print(con_doc)
+        else:
+            final_query = f"質問: {query}"
 
         return self.chain.run(input=final_query)
 
@@ -84,10 +93,11 @@ class ZundaOracleExtraMessage(ExtraMessage):
 
     async def give_knowledge_command(self, inter: discord.Interaction, knowledge: str):
         docsearch = FAISS.from_texts(texts=[knowledge], embedding=self.embedding)
-        if self.faiss_indexes is None:
-            self.faiss_indexes = docsearch
+        guild = inter.guild.id
+        if self.faiss_indexes.get(guild) is None:
+            self.faiss_indexes[guild] = docsearch
         else:
-            self.faiss_indexes.merge_from(docsearch)
+            self.faiss_indexes[guild].merge_from(docsearch)
 
-        self.faiss_indexes.save_local('zunda_oracle.faiss')
+        self.faiss_indexes[guild].save_local(f"zunda_oracle.faiss/{guild}")
         await inter.response.send_message("おぼえたのだ！")
